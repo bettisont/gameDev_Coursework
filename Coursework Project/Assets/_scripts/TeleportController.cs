0e93ed0cfd662db34d21059d5c84d51fadc3f3c7
Tim Bettison <bettisont@gmail.com>
2020-03-03 21:50:54 +0000

    added teleport functionality for level 1

---
+++ Coursework Project/Assets/_scripts/TeleportController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = teleportTarget.transform.position;
+    }
+}
